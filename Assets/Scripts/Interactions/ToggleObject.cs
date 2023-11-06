1adfcfff67fa3969c127e2a6864c3083e90fb389
mikepmerritt <mikepmerritt@gmail.com>
2023-11-06 06:20:13 -0500

    made object attached to toggle script disable when player is too far away

--- Assets/Scripts/Interactions/ToggleObject.cs
+++ Assets/Scripts/Interactions/ToggleObject.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class ToggleObject : MonoBehaviour
 {
     private bool IsActive = false;
     [SerializeField] private GameObject ObjectToEnable;
+    [SerializeField] private float DistanceToDisable;
+    private GameObject Player;
 
     public void ToggleObjectEnabled()
     {
         IsActive = !IsActive;
         ObjectToEnable.SetActive(IsActive);
-        Debug.Log("called");
+    }
+
+    private void Start()
+    {
+        Player = FindObjectOfType<XROrigin>().gameObject;
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(Player.transform.position, transform.position) > DistanceToDisable)
+        {
+            IsActive = false;
+            ObjectToEnable.SetActive(false);
+        }
     }
 }
