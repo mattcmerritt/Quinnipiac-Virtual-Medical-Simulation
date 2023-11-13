bf410de324f1fc432384115bd611367964e9f478
mattcmerritt <mattcmerritt@gmail.com>
2023-11-13 05:20:00 -0500

    support for scaling at runtime

--- Assets/Scripts/Simulation Builder/SizeFitter.cs
+++ Assets/Scripts/Simulation Builder/SizeFitter.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Metadata;
 
 public class SizeFitter : MonoBehaviour
 {
-    // NOTE: THIS SHOULD ONLY BE FALSE IF THE GAME OBJECT IS ALREADY CENTERED
-    //       IF THE COMPONENTS ARE NOT BALANCED, THE BOX WILL NOT FIT
-    [SerializeField] private bool Recenter = true;
     [SerializeField] private bool ShowDebug = false;
+    [SerializeField] private bool UpdateRealtime = false;
 
-    public void Start()
+    private void Start()
     {
+        GenerateShape();
+    }
+
+    private void Update()
+    {
+        if (UpdateRealtime) GenerateShape();
+    }
+
+    public void GenerateShape()
+    {
+        UpdateBounds();
+    }
+
+    private void UpdateBounds()
+    {
+        // Getting size of bounding shape
         RectTransform children = transform.GetComponentInChildren<RectTransform>();
 
         float minX = Mathf.Infinity;
@@ -51,22 +66,20 @@ public class SizeFitter : MonoBehaviour
 
         if (ShowDebug) Debug.Log($"Final: min: ({minX}, {minY}), max: ({maxX}, {maxY})");
 
-        if (Recenter)
-        {
-            float centerX = (maxX + minX) / 2f;
-            float centerY = (maxY + minY) / 2f;
-
-            if (ShowDebug) Debug.Log($"Center: ({centerX}, {centerY})");
-
-            foreach (RectTransform child in children)
-            {
-                string result = $"{child.name} Old: {child.localPosition}";
-                child.localPosition -= new Vector3(centerX, centerY);
-                result += $" New: {child.localPosition}";
-                if (ShowDebug) Debug.Log(result);
-            }
-        }
-
         GetComponent<RectTransform>().sizeDelta = new Vector2(maxX - minX, maxY - minY);
+
+        // Centering
+        float centerX = (maxX + minX) / 2f;
+        float centerY = (maxY + minY) / 2f;
+
+        if (ShowDebug) Debug.Log($"Center: ({centerX}, {centerY})");
+
+        foreach (RectTransform child in children)
+        {
+            string result = $"{child.name} Old: {child.localPosition}";
+            child.localPosition -= new Vector3(centerX, centerY);
+            result += $" New: {child.localPosition}";
+            if (ShowDebug) Debug.Log(result);
+        }
     }
 }
